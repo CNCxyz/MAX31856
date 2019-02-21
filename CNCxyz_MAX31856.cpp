7d8dfa3436822f828aee3c24ce3fd489ee5943ab
gordiushenkov <gordyushenkov@gmail.com>
2019-02-22 00:48:30 +0300

    Basic functionality implemented: read temperatures, set/get filter option, averaging mode, thermocouple type

--- CNCxyz_MAX31856.cpp
+++ CNCxyz_MAX31856.cpp
@@ -3,140 +3,332 @@
 #include <stdlib.h>
 #include <SPI.h>
 
-const SPISettings = SPISettings(500000, MSBFIRST, SPI_MODE1);
+static const SPISettings settings = SPISettings(500000, MSBFIRST, SPI_MODE1);
+static const uint16_t T_CONV_60Hz_ms = 155;
+static const uint16_t T_CONV_50Hz_ms = 185;
 
-boolean CNCxyz_MAX31856::begin(void) {
-	pinMode(_cs, OUTPUT);
-	digitalWrite(_cs, HIGH);
-
-	if (_sck != -1) {
-		pinMode(_sck, OUTPUT);
-		pinMode(_mosi, OUTPUT);
-		pinMode(_miso, INPUT);
-	} else {
-		SPI.begin();
-	}
-
-	return true;
+/**
+    @brief  Basic constructor
+    @param  cs [in]: pin used for CS signal
+    @retval None
+*/
+CNCxyz_MAX31856::CNCxyz_MAX31856(const int8_t cs) : _cs(cs), 
+_sck(-1), _miso(-1), _mosi(-1), _tc_type(MAX31856_TC_TYPE_K) {
 }
 
-// If using hardware SPI, all we need is the chipselect pin.
-CNCxyz_MAX31856::CNCxyz_MAX31856(int8_t cs) {
-	_cs = cs;
-	_sck = _miso = _mosi = -1;
-	_tc_type = MAX31856_TC_TYPE_K;
+/**
+    @brief  Constructor with thermocouple definition
+    @param  cs [in]: pin used for CS signal
+    @param  tc [in]: thremocouple type
+    @retval None
+*/
+CNCxyz_MAX31856::CNCxyz_MAX31856(const int8_t cs, const MAX31856_TC_TYPE_MaskT tc) :
+  _cs(cs), _tc_type(tc), _sck(-1), _miso(-1), _mosi(-1) {
 }
 
-// We can also specify the Thermocouple type.
-CNCxyz_MAX31856::CNCxyz_MAX31856(int8_t cs, int8_t tc) {
-	_cs = cs;
-	_sck = _miso = _mosi = -1;
-	_tc_type = tc;
+/**
+    @brief  Constructor for software SPI
+    @param  cs [in]: pin used for CS signal
+    @param  mosi [in]: pin used for MOSI signal
+    @param  miso [in]: pin used for MISO signal
+    @param  sck [in]: pin used for SCK signal
+    @retval None
+*/
+CNCxyz_MAX31856::CNCxyz_MAX31856(const int8_t cs, const int8_t mosi,
+  const int8_t miso, const int8_t sck) : _cs(cs), _sck(sck), _miso(miso), _mosi(mosi),
+  _tc_type(MAX31856_TC_TYPE_K) {  
 }
 
-// If using software SPI, we need all pins for bitbanging.
-CNCxyz_MAX31856::CNCxyz_MAX31856(int8_t cs, int8_t mosi, int8_t miso, int8_t sck) {
-	_sck = sck;
-	_cs = cs;
-	_miso = miso;
-	_mosi = mosi;
-	_tc_type = MAX31856_TC_TYPE_K;
+/**
+    @brief  Constructor for software SPI with thermocouple definition
+    @param  cs [in]: pin used for CS signal
+    @param  mosi [in]: pin used for MOSI signal
+    @param  miso [in]: pin used for MISO signal
+    @param  sck [in]: pin used for SCK signal
+    @param  tc [in]: thremocouple type
+    @retval None
+*/
+CNCxyz_MAX31856::CNCxyz_MAX31856(const int8_t cs, const int8_t mosi,
+  const int8_t miso, const int8_t sck, const MAX31856_TC_TYPE_MaskT tc) :
+  _cs(cs), _mosi(mosi), _miso(miso), _sck(sck), _tc_type(tc) {
 }
 
-// We can also specify the Thermocouple type.
-CNCxyz_MAX31856::CNCxyz_MAX31856(int8_t cs, int8_t mosi, int8_t miso, int8_t sck, int8_t tc) {
-	_sck = sck;
-	_cs = cs;
-	_miso = miso;
-	_mosi = mosi;
-	_tc_type = tc;
+/**
+    @brief  Hardware configuration
+    @param  None
+    @retval None
+*/
+void CNCxyz_MAX31856::begin(void) {
+  pinMode(_cs, OUTPUT);
+  digitalWrite(_cs, HIGH);
+
+  if (_sck != -1) {
+    pinMode(_sck, OUTPUT);
+    pinMode(_mosi, OUTPUT);
+    pinMode(_miso, INPUT);
+  } else {
+    SPI.begin();
+  }
+  setThermocoupleType(_tc_type);
 }
 
-void CNCxyz_MAX31856::setThermocoupleType(int8_t tc){
-	uint8_t x = read(MAX31856_REG_CR1, 1);
-	x &= 0xF0;
-	x |= (uint8_t)tc & 0x0F;
-	write(MAX31856_REG_CR1, x);
+/**
+    @brief  Setting thermocouple type
+    @param  tc [in]: thremocouple type
+    @retval None
+*/
+void CNCxyz_MAX31856::setThermocoupleType(const MAX31856_TC_TYPE_MaskT tc) {
+  uint8_t CR1 = read(MAX31856_REG_CR1);
+  CR1 &= 0xF0;
+  CR1 |= (uint8_t)tc;
+  write(MAX31856_REG_CR1, CR1);
 }
 
-uint8_t CNCxyz_MAX31856::getThermocoupleType(){
-	uint8_t x = read(MAX31856_REG_CR1, 1);
-	x &= 0x0F;
-	return x;
+/**
+    @brief  Getting thermocouple type
+    @param  None
+    @retval Thermocouple type
+*/
+MAX31856_TC_TYPE_MaskT CNCxyz_MAX31856::getThermocoupleType(void) {
+  MAX31856_TC_TYPE_MaskT x = (MAX31856_TC_TYPE_MaskT)(read(MAX31856_REG_CR1) & 0x0F);
+  return x;
 }
 
-float CNCxyz__MAX31856::readThermocouple(void) {
+/**
+    @brief  Reads hot junction temperature
+    @param  None
+    @retval Hot junction temperature in Celsius degrees
+*/
+float CNCxyz_MAX31856::readThermocouple(void) {
+  convert(); // Perform single conversion
 
+  // Read Linearized TC temperature registers
+  uint8_t buf[3];
+  readMultiple(MAX31856_REG_LTCBH, buf, 3);
+
+  // Converting linearized TC temperature code to real temperature
+  int32_t temp_code = ((int32_t)buf[0] << 16) | ((int32_t)buf[1] << 8) | ((int32_t)buf[2] & 0xE0);
+  if (temp_code & 0x800000) {
+    temp_code |= 0xFF000000;
+  }
+  temp_code >>= 5;
+  float temperature = (float) temp_code / (1 << 7);
+
+  return temperature;
 }
 
+/**
+    @brief  Reads cold junction temperature
+    @param  None
+    @retval Cold junction temperature in Celsius degrees
+*/
 float CNCxyz_MAX31856::readColdJunction(void) {
+  convert(); // Perform single conversion
 
+  // Reading cold-junction temperature registers
+  uint8_t buf[2];
+  readMultiple(MAX31856_REG_CJTH, buf, 2);
+
+  // Converting code to real temperature
+  int16_t temp_code = (int16_t)buf[0] << 8 + (int16_t)buf[1];
+  float temperature = (float)temp_code / (1 << 8);
+
+  return temperature;
 }
 
-uint8_t CNCxyz_MAX31856::readFault(void) {
-	return read(MAX31856_REG_SR, 1);
+/**
+    @brief  Sets thermocouple voltage conversion averaging mode
+    @param  avgMask[in] : number of samples to be averaged during conversion
+    @retval None
+*/
+void CNCxyz_MAX31856::setAvergingMode(const MAX31856_AVGSEL_MaskT avgMask) {
+  uint8_t CR1 = read(MAX31856_REG_CR1);
+  CR1 &= 0x8F;
+  CR1 |= (uint8_t)avgMask;
+  write(MAX31856_REG_CR1, CR1);
 }
 
-uint8_t CNCxyz_MAX31856::read(uint8_t address, uint8_t byte){
+/**
+    @brief  Gets thermocouple voltage conversion averaging mode
+    @param  None
+    @retval Number of samples used in one conversion
+*/
+uint8_t CNCxyz_MAX31856::getAvergingMode(void) {
+  // Read the bitfield
+  uint8_t avgMode = (read(MAX31856_REG_CR1) >> 4) & 0x07;
 
-	//Start Transfer, Open Connection
-	if (_sck == -1) {
-		SPI.beginTransaction(SPISettings);
-	}else {
-		digitalWrite(_sck, HIGH);
-	}
-	digitalWrite(_cs, LOW);
-
-	transfer(address);
-	out = transfer(byte);
-
-	//End Transfer, Close Connection for other programs to use.
-	if (_sck == -1) {
-		SPI.endTransaction();
-	}
-
-	digitalWrite(_cs, HIGH);
-	return out;
-
+  // Calculate the number of samples
+  uint8_t nSamples = 0;
+  if (avgMode > 3) {
+    nSamples = 16;
+  } else {
+    nSamples = 1 << avgMode;
+  }
+  return nSamples;
 }
 
-uint8_t CNCxyz_MAX31856::write(uint8_t address, uint8_t data){
+/**
+    @brief  Sets noise rejection filter
+    @param  filter[in] : filter option
+    @retval None
+*/
+void CNCxyz_MAX31856::setNoiseFilter(const MAX31856_FilterT filter) {
+  uint8_t CR0 = read(MAX31856_REG_CR0);
+  CR0 &= ~0x01;
 
-	//Start Transfer, Open Connection
-	if (_sck == -1) {
-		SPI.beginTransaction(SPISettings);
-	}else {
-		digitalWrite(_sck, HIGH);
-	}
-	digitalWrite(_cs, LOW);
+  // Set flag for 50Hz filter
+  if (MAX31856_NoiseFilter50Hz == filter) {
+    CR0 |= 0x01;
+  }
 
-	transfer(address);
-	transfer(data);
-
-	//End Transfer, Close Connection for other programs to use.
-	if (_sck == -1) {
-		SPI.endTransaction();
-	}
-	digitalWrite(_cs, HIGH);
-
+  write(MAX31856_REG_CR0, CR0);
 }
 
-uint8_t CNCxyz_MAX31856::transfer(uint8_t val) {
+/**
+    @brief  Gets moise rejection filter option
+    @param  None
+    @retval Noise rejection filter option
+*/
+MAX31856_FilterT CNCxyz_MAX31856::getNoiseFilter(void) {
+  uint8_t filterFlag = read(MAX31856_REG_CR0) & 0x01;
 
-	//If Hardware SPI, use default SPI class.
-	if (_sck == -1)
-		return SPI.transfer(val);
+  return filterFlag ? MAX31856_NoiseFilter50Hz : MAX31856_NoiseFilter60Hz;
+}
 
-	//If Software SPI, bitbang our way to glory.
-	uint8_t out = 0;
-	uint8_t bval = 0;
-	for (uint8_t bit = 0; bit < 8; bit++) {
-		digitalWrite(_sck, HIGH);
-		bval = digitalRead(_miso);
-		out >>= 1;
-		out |= bval << 7;
-		digitalWrite(_sck, LOW);
-		digitalWrite(_mosi, val & (1 << bit) ? HIGH : LOW);
-	}
-	return out;
+//------------------------------ Private functions ----------------------------
+/**
+    @brief  Read MAX31856 register
+    @param  address [in]: register address
+    @retval Register value
+*/
+uint8_t CNCxyz_MAX31856::read(const MAX31856_addressT address) {
+  uint8_t value;
+  readMultiple(address, &value, 1);
+  return value;
+}
+
+/**
+    @brief  Read MAX31856 multiple registers
+    @param  address [in]: register address
+    @param  rx_buf [out]: receiving buffer pointer
+    @param  size [in]: number of bytes to read
+    @retval None
+*/
+void CNCxyz_MAX31856::readMultiple(const MAX31856_addressT address, uint8_t* const rx_buf, const uint8_t size) {
+  //Start Transfer, Open Connection
+  if (_sck == -1) {
+    SPI.beginTransaction(settings);
+  } else {
+    digitalWrite(_sck, HIGH);
+  }
+  digitalWrite(_cs, LOW);
+
+  // Send address and read specified number of bytes
+  transfer(address);
+  for (uint8_t i = 0; i < size; ++i) {
+    rx_buf[i] = transfer(0xFF);
+  }
+
+  //End Transfer, Close Connection for other programs to use.
+  if (_sck == -1) {
+    SPI.endTransaction();
+  }
+  digitalWrite(_cs, HIGH);
+}
+
+/**
+    @brief  Write MAX31856 register
+    @param  address [in]: register address
+    @param  value [in]: register value
+    @retval None
+*/
+void CNCxyz_MAX31856::write(const MAX31856_addressT address, const uint8_t value) {
+  writeMultiple(address, &value, 1);
+}
+
+/**
+    @brief  Write MAX31856 register
+    @param  address [in]: register address
+    @param  tx_buf [in]: data buffer pointer
+    @param  size [in]: number of bytes to write
+    @retval None
+*/
+void CNCxyz_MAX31856::writeMultiple(const MAX31856_addressT address, 
+  const uint8_t* const tx_buf, const uint8_t size) {
+
+  //Start Transfer, Open Connection
+  if (_sck == -1) {
+    SPI.beginTransaction(settings);
+  } else {
+    digitalWrite(_sck, HIGH);
+  }
+  digitalWrite(_cs, LOW);
+
+  // Send address and specified number of bytes
+  transfer(address | 0x80);
+  for (uint8_t i = 0; i < size; ++i) {
+    transfer(tx_buf[i]);
+  }
+
+  //End Transfer, Close Connection for other programs to use.
+  if (_sck == -1) {
+    SPI.endTransaction();
+  }
+  digitalWrite(_cs, HIGH);
+}
+
+/**
+    @brief  SPI byte transfer
+    @param  val [in]: byte to transfer
+    @retval Received byte
+*/
+uint8_t CNCxyz_MAX31856::transfer(const uint8_t val) {
+
+  //If Hardware SPI, use default SPI class.
+  if (_sck == -1)
+    return SPI.transfer(val);
+
+  //If Software SPI, bitbang our way to glory.
+  uint8_t out = 0;
+  uint8_t bval = 0;
+  for (uint8_t bit = 0; bit < 8; bit++) {
+    digitalWrite(_sck, HIGH);
+    bval = digitalRead(_miso);
+    out >>= 1;
+    out |= bval << 7;
+    digitalWrite(_sck, LOW);
+    digitalWrite(_mosi, val & (1 << bit) ? HIGH : LOW);
+  }
+  return out;
+}
+
+/**
+    @brief  Execute temperature conversion
+    @param  None
+    @retval None
+*/
+void CNCxyz_MAX31856::convert(void) {
+  // Get current settings  
+  uint16_t convertionTime_ms;
+  uint8_t samples = getAvergingMode();
+  MAX31856_FilterT noiseFilter = getNoiseFilter();
+  
+  // Start single conversion
+  uint8_t CR0 = read(MAX31856_REG_CR0);
+  CR0 &= ~MAX31856_REG_CR0_AUTOCONVERT;
+  CR0 |= MAX31856_REG_CR0_1SHOT;
+  write(MAX31856_REG_CR0, CR0);
+
+  // Calculate conversion time(p.20)
+  switch (noiseFilter) {
+    case MAX31856_NoiseFilter60Hz:
+      convertionTime_ms = T_CONV_60Hz_ms + (samples - 1) * 34;
+      break;
+    case MAX31856_NoiseFilter50Hz:
+      convertionTime_ms = T_CONV_50Hz_ms + (samples - 1) * 40;
+      break;
+  }
+
+  // Wait until the end of conversion
+  delay(convertionTime_ms);
 }
--- CNCxyz_MAX31856.h
+++ CNCxyz_MAX31856.h
@@ -5,61 +5,109 @@
 #include "Arduino.h"
 
 // Register Memory Map (Datasheet Page 18)
-#define MAX31856_REG_CR0				0x00 // Configuration 0 Register
-#define MAX31856_REG_CR1				0x01 // Configuration 1 Register
-#define MAX31856_REG_MASK				0x02 // Fault Mask Register
-#define MAX31856_REG_CJHF				0x03 // Cold-Junction High Fault Threshold
-#define MAX31856_REG_CJLF				0x04 // Cold-Junction Low Fault Threshold
-#define MAX31856_REG_LTHFTH				0x05 // Linearized Temperature High Fault Threshold MSB
-#define MAX31856_REG_LTHFTL				0x06 // Linearized Temperature High Fault Threshold LSB
-#define MAX31856_REG_LTLFTH				0x07 // Linearized Temperature Low Fault Threshold MSB
-#define MAX31856_REG_LTLFTL				0x08 // Linearized Temperature Low Fault Threshold LSB
-#define MAX31856_REG_CJTO				0x09 // Cold-Junction Temperature Offset Register
-#define MAX31856_REG_CJTH				0x0A // Cold-Junction Temperature Register, MSB
-#define MAX31856_REG_CJTL				0x0B // Cold-Junction Temperature Register, LSB
-#define MAX31856_REG_LTCBH				0x0C // Linearized TC Temperature, Byte 2 (Read Only)
-#define MAX31856_REG_LTCBM				0x0D // Linearized TC Temperature, Byte 1 (Read Only)
-#define MAX31856_REG_LTCBL				0x0E // Linearized TC Temperature, Byte 0 (Read Only)
-#define MAX31856_REG_SR					0x0F // Fault Status Register (Read Only)
+typedef enum {
+  MAX31856_REG_CR0			=	0x00, // Configuration 0 Register
+  MAX31856_REG_CR1			=	0x01, // Configuration 1 Register
+  MAX31856_REG_MASK			=	0x02, // Fault Mask Register
+  MAX31856_REG_CJHF			=	0x03, // Cold-Junction High Fault Threshold
+  MAX31856_REG_CJLF			=	0x04, // Cold-Junction Low Fault Threshold
+  MAX31856_REG_LTHFTH		=	0x05, // Linearized Temperature High Fault Threshold MSB
+  MAX31856_REG_LTHFTL		=	0x06, // Linearized Temperature High Fault Threshold LSB
+  MAX31856_REG_LTLFTH		=	0x07, // Linearized Temperature Low Fault Threshold MSB
+  MAX31856_REG_LTLFTL		=	0x08, // Linearized Temperature Low Fault Threshold LSB
+  MAX31856_REG_CJTO			=	0x09, // Cold-Junction Temperature Offset Register
+  MAX31856_REG_CJTH			=	0x0A, // Cold-Junction Temperature Register, MSB
+  MAX31856_REG_CJTL			=	0x0B, // Cold-Junction Temperature Register, LSB
+  MAX31856_REG_LTCBH		=	0x0C, // Linearized TC Temperature, Byte 2 (Read Only)
+  MAX31856_REG_LTCBM		=	0x0D, // Linearized TC Temperature, Byte 1 (Read Only)
+  MAX31856_REG_LTCBL		=	0x0E, // Linearized TC Temperature, Byte 0 (Read Only)
+  MAX31856_REG_SR				=	0x0F, // Fault Status Register (Read Only)
+} MAX31856_addressT;
 
-// Configuration 0 Register (Datasheet Page 19)
-#define MAX31856_REG_CR0_AUTOCONVERT	0x80 // Configuration 0 Conversion Mode
-#define MAX31856_REG_CR0_1SHOT			0x40 // Configuration 0 One-Shot Mode
-#define MAX31856_REG_CR0_OCFAULT1		0x20 // Configuration 0 Open-Circuit Fault Detection 1
-#define MAX31856_REG_CR0_OCFAULT0		0x10 // Configuration 0 Open-Circuit Fault Detection 0
-#define MAX31856_REG_CR0_CJ				0x08 // Configuration 0 Cold-Junction Sensor Disable
-#define MAX31856_REG_CR0_FAULT			0x04 // Configuration 0 Fault Mode
-#define MAX31856_REG_CR0_FAULTCLR		0x02 // Configuration 0 Fault Status Clear
-#define MAX31856_REG_CR0_NOISE_FILTER	0x01 // Configuration 0 50Hz/60Hz Noise Rejection Filter Selection
+// Configuration 0 Register masks (Datasheet Page 19)
+typedef enum {
+  MAX31856_REG_CR0_AUTOCONVERT	= 0x80, // Configuration 0 Conversion Mode
+  MAX31856_REG_CR0_1SHOT			  = 0x40, // Configuration 0 One-Shot Mode
+  MAX31856_REG_CR0_OCFAULT1		  = 0x20, // Configuration 0 Open-Circuit Fault Detection 1
+  MAX31856_REG_CR0_OCFAULT0		  = 0x10, // Configuration 0 Open-Circuit Fault Detection 0
+  MAX31856_REG_CR0_CJ				    = 0x08, // Configuration 0 Cold-Junction Sensor Disable
+  MAX31856_REG_CR0_FAULT			  = 0x04, // Configuration 0 Fault Mode
+  MAX31856_REG_CR0_FAULTCLR		  = 0x02, // Configuration 0 Fault Status Clear
+  MAX31856_REG_CR0_NOISE_FILTER	= 0x01, // Configuration 0 50Hz/60Hz Noise Rejection Filter Selection
+} MAX31856_CR0_MaskT;
 
 // Averaging Mode (Datasheet Page 20)
-#define MAX31856_AVGSEL_1				0b000 // Default
-#define MAX31856_AVGSEL_2				0b001
-#define MAX31856_AVGSEL_4				0b010
-#define MAX31856_AVGSEL_8				0b011
-#define MAX31856_AVGSEL_16				0b100
+typedef enum {
+  MAX31856_AVG_NSAMPLES_1			=	0b000, // Default
+  MAX31856_AVG_NSAMPLES_2			=	0b001,
+  MAX31856_AVG_NSAMPLES_4			=	0b010,
+  MAX31856_AVG_NSAMPLES_8			=	0b011,
+  MAX31856_AVG_NSAMPLES_16		=	0b100,
+} MAX31856_AVGSEL_MaskT;
 
 // Thermocouple Types (Datasheet Page 20)
-#define MAX31856_TC_TYPE_B				0b0000
-#define MAX31856_TC_TYPE_E				0b0001
-#define MAX31856_TC_TYPE_J				0b0010
-#define MAX31856_TC_TYPE_K				0b0011 // Default
-#define MAX31856_TC_TYPE_N				0b0100
-#define MAX31856_TC_TYPE_R				0b0101
-#define MAX31856_TC_TYPE_S				0b0110
-#define MAX31856_TC_TYPE_T				0b0111
-#define MAX31856_VMODE_8				0b1000 // Voltage Mode, Gain = 8. Code = 8 x 1.6 x 217 x VIN
-#define MAX31856_VMODE_32				0b1100 // Voltage Mode, Gain = 32. Code = 32 x 1.6 x 217 x VIN
+typedef enum {
+  MAX31856_TC_TYPE_B			=	0b0000,
+  MAX31856_TC_TYPE_E			=	0b0001,
+  MAX31856_TC_TYPE_J			=	0b0010,
+  MAX31856_TC_TYPE_K			=	0b0011, // Default
+  MAX31856_TC_TYPE_N			=	0b0100,
+  MAX31856_TC_TYPE_R			=	0b0101,
+  MAX31856_TC_TYPE_S			=	0b0110,
+  MAX31856_TC_TYPE_T			=	0b0111,
+  MAX31856_VMODE_8				= 0b1000, // Voltage Mode, Gain = 8. Code = 8 x 1.6 x 217 x VIN
+  MAX31856_VMODE_32				= 0b1100, // Voltage Mode, Gain = 32. Code = 32 x 1.6 x 217 x VIN
+} MAX31856_TC_TYPE_MaskT;
 
 // Fault Mask Register (Datasheet Page 21)
-#define MAX31856_FAULT_CJRANGE			0x80 // Cold Junction Out-of-Range
-#define MAX31856_FAULT_TCRANGE			0x40 // Thermocouple Out-of-Range
-#define MAX31856_FAULT_CJHIGH			0x20 // Cold-Junction High Fault
-#define MAX31856_FAULT_CJLOW			0x10 // Cold-Junction Low Fault
-#define MAX31856_FAULT_TCHIGH			0x08 // Thermocouple Temperature High Fault
-#define MAX31856_FAULT_TCLOW			0x04 // Thermocouple Temperature Low Fault
-#define MAX31856_FAULT_OVUV				0x02 // Overvoltage or Undervoltage Input Fault
-#define MAX31856_FAULT_OPEN				0x01 // Thermocouple Open-Circuit Fault
+typedef enum {
+  MAX31856_FAULT_CJRANGE		=	0x80, // Cold Junction Out-of-Range
+  MAX31856_FAULT_TCRANGE		=	0x40, // Thermocouple Out-of-Range
+  MAX31856_FAULT_CJHIGH			= 0x20, // Cold-Junction High Fault
+  MAX31856_FAULT_CJLOW			= 0x10, // Cold-Junction Low Fault
+  MAX31856_FAULT_TCHIGH			= 0x08, // Thermocouple Temperature High Fault
+  MAX31856_FAULT_TCLOW			= 0x04, // Thermocouple Temperature Low Fault
+  MAX31856_FAULT_OVUV				= 0x02, // Overvoltage or Undervoltage Input Fault
+  MAX31856_FAULT_OPEN				= 0x01, // Thermocouple Open-Circuit Fault
+} MAX31856_Fault_MaskT;
 
+// Noise filter option
+typedef enum {
+  MAX31856_NoiseFilter60Hz = 0x00,  // Selects rejection of 60Hz and its harmonics
+  MAX31856_NoiseFilter50Hz = 0x01,  // Selects rejection of 50Hz and its harmonics
+} MAX31856_FilterT;
+
+class CNCxyz_MAX31856 {
+public:
+  CNCxyz_MAX31856(const int8_t cs);
+  CNCxyz_MAX31856(const int8_t cs, const MAX31856_TC_TYPE_MaskT tc);
+  CNCxyz_MAX31856(const int8_t cs, const int8_t mosi, const int8_t miso, const int8_t sck);
+  CNCxyz_MAX31856(const int8_t cs, const int8_t mosi, const int8_t miso, const int8_t sck, 
+    const MAX31856_TC_TYPE_MaskT tc);
+  void begin(void);
+  void setThermocoupleType(const MAX31856_TC_TYPE_MaskT tc);
+  MAX31856_TC_TYPE_MaskT getThermocoupleType(void);
+  float readThermocouple(void);
+  float readColdJunction(void);
+  void setAvergingMode(const MAX31856_AVGSEL_MaskT avgMask);
+  uint8_t getAvergingMode(void);
+  void setNoiseFilter(const MAX31856_FilterT filter);
+  MAX31856_FilterT getNoiseFilter(void);
+
+private:
+  int8_t _cs;
+  int8_t _sck;
+  int8_t _miso;
+  int8_t _mosi;
+  MAX31856_TC_TYPE_MaskT _tc_type;
+
+  uint8_t read(const MAX31856_addressT address);
+  void readMultiple(const MAX31856_addressT address, uint8_t* const rx_buf, const uint8_t size);
+  void write(const MAX31856_addressT address, const uint8_t value);
+  void writeMultiple(const MAX31856_addressT address, const uint8_t* const tx_buf, 
+    const uint8_t size);
+  uint8_t transfer(const uint8_t val);
+  void convert(void);
+};
 
 #endif
